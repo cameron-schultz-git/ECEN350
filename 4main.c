f465e92e1c3acbdab474b6c59b94d0f33313e28d
Cameron Schultz <cameron.schultzsloan@gmail.com>
2026-09-25 12:19:19 -0500

    Simplify power function test cases
    Reduced test cases for power function to a single base and exponent.

--- 4main.c
+++ 4main.c
@@ -10,9 +10,8 @@ int main(void)
     long long int a = my_mul(3, 5);
     printf("Result of my_mul(3, 5) = %lld\n", a);
 
-    /* test power with several base/exp pairs */
-    long long int bases[] = {2, 3, 5, 7, 10, 1, 0, 4};
-    long long int exps[]  = {10, 4, 3, 0, 5, 8, 3, 1};
+    long long int bases[] = {2};
+    long long int exps[]  = {2};
     int n = sizeof(bases) / sizeof(bases[0]);
 
     for (int i = 0; i < n; i++) {
